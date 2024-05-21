ef67d1014ac95382cbd46a0e4bf223ddf9d5cfc7
barbaric-ex <v.barbaric@studio-exit.com>
2024-05-21 14:18:14 +0200

    21.5

--- page-home.php
+++ page-home.php
@@ -114,15 +114,528 @@ get_header(); ?>
         <div class="row">
             <div class="col-lg-12">
                 <div class="wrapper">
+
+
+
+
+                    <div class="image">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/home_content/Home/home-pic.jpg" alt="">
+
+                    </div>
+                    <div class="line">
+                        <div class="inner_red"></div>
+                    </div>
                     <div class="global_title">
-                        <h2>How We Can Help You</h2>
+                        <h2 class="line1">How We Can</h2>
+                        <h2 class="line2">Help You</h2>
                     </div>
                 </div>
             </div>
+
+            <div class="col-lg-6">
+                <div class="wrap">
+                    <div class="line">
+                        <div class="inner_red"></div>
+                    </div>
+
+                    <div class="number_wrap">
+                        <div class="number">01</div>
+                    </div>
+
+                    <div class="text_wrap">
+                        <div class="title">
+                            <h3>Druckguss</h3>
+                        </div>
+                        <div class="text">
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+            <div class="col-lg-6">
+                <div class="wrap">
+                    <div class="line">
+                        <div class="inner_red"></div>
+                    </div>
+
+                    <div class="number_wrap">
+                        <div class="number">02</div>
+                    </div>
+
+                    <div class="text_wrap">
+                        <div class="title">
+                            <h3>Recycling / Alloying</h3>
+                        </div>
+                        <div class="text">
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+            <div class="col-lg-6">
+                <div class="wrap">
+                    <div class="line">
+                        <div class="inner_red"></div>
+                    </div>
+
+                    <div class="number_wrap">
+                        <div class="number">03</div>
+                    </div>
+
+                    <div class="text_wrap">
+                        <div class="title">
+                            <h3>Sandguss</h3>
+                        </div>
+                        <div class="text">
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+            <div class="col-lg-6">
+                <div class="wrap">
+                    <div class="line">
+                        <div class="inner_red"></div>
+                    </div>
+
+                    <div class="number_wrap">
+                        <div class="number">04</div>
+                    </div>
+
+                    <div class="text_wrap">
+                        <div class="title">
+                            <h3>Customer Engineering</h3>
+                        </div>
+                        <div class="text">
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-lg-6">
+                <div class="wrap">
+                    <div class="line">
+                        <div class="inner_red"></div>
+                    </div>
+
+                    <div class="number_wrap">
+                        <div class="number">05</div>
+                    </div>
+
+                    <div class="text_wrap">
+                        <div class="title">
+                            <h3>Schutzgasmischanlage</h3>
+                        </div>
+                        <div class="text">
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+            <div class="col-lg-6">
+                <div class="wrap wrap_video">
+                    <div class="video-wrap">
+                        <video disableRemotePlayback loop playsinline muted autoplay="">
+                            <source src="<?php echo get_template_directory_uri(); ?>/img/home_content/Home/01_rauch_innovation_3.mp4" type="video/mp4">
+                        </video>
+                    </div>
+
+                </div>
+            </div>
+
+
+
         </div>
     </div>
 </div>
 
+
+<div class="home_sec4">
+    <div class="container">
+
+        <div class="wrapp1">
+            <div class="arows_wrap">
+                <div class="arrow arrow1">Prev</div>
+                <div class="arrow arrow2">Next</div>
+            </div>
+            <div class="global_title">
+                <h2 class="line1">Your</h2>
+                <h2 class="line2">Values</h2>
+            </div>
+
+        </div>
+
+        <div class="slider_wrap">
+            <div class="owl-carousel image_slider1 owl-theme">
+
+                <div class="item_box">
+                    <div class="image">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/home_content/Home/your-benefit-preview-trust.jpg" alt="">
+
+                    </div>
+
+                    <div class="content">
+                        <div class="title">
+                            <h2>WE MELT<br> WITH TRUST</h2>
+                            <h3>Vertrauen</h3>
+                        </div>
+
+                        <div class="text">
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item_box">
+                    <div class="image">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/home_content/Home/your-benefit-preview-joy.jpg" alt="">
+
+                    </div>
+
+                    <div class="content">
+                        <div class="title">
+                            <h2>WE MELT<br> WITH TRUST</h2>
+                            <h3>Vertrauen</h3>
+                        </div>
+
+                        <div class="text">
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item_box">
+                    <div class="image">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/home_content/Home/your-benefit-preview-innovation.jpg" alt="">
+
+                    </div>
+
+                    <div class="content">
+                        <div class="title">
+                            <h2>WE MELT<br> WITH TRUST</h2>
+                            <h3>Vertrauen</h3>
+                        </div>
+
+                        <div class="text">
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item_box">
+                    <div class="image">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/home_content/Home/your-benefit-preview-credibility.jpg" alt="">
+
+                    </div>
+
+                    <div class="content">
+                        <div class="title">
+                            <h2>WE MELT<br> WITH TRUST</h2>
+                            <h3>Vertrauen</h3>
+                        </div>
+
+                        <div class="text">
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item_box">
+                    <div class="image">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/home_content/Home/your-benefit-preview-safety.jpg" alt="">
+
+                    </div>
+
+                    <div class="content">
+                        <div class="title">
+                            <h2>WE MELT<br> WITH TRUST</h2>
+                            <h3>Vertrauen</h3>
+                        </div>
+
+                        <div class="text">
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="item_box">
+                    <div class="image">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/home_content/Home/your-benefit-preview-respect-and-community.jpg" alt="">
+
+                    </div>
+
+                    <div class="content">
+                        <div class="title">
+                            <h2>WE MELT<br> WITH TRUST</h2>
+                            <h3>Vertrauen</h3>
+                        </div>
+
+                        <div class="text">
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+
+    </div>
+</div>
+
+<script>
+    $(window).on('load', function() {
+        let owl2 = $('.owl-carousel.image_slider1').owlCarousel({
+
+            dots: true,
+            loop: true,
+            margin: 50,
+            autoWidth: true,
+
+
+
+        });
+
+        $('.home_sec4 .wrapp1 .arows_wrap .arrow.arrow1').click(function() {
+            owl2.trigger('prev.owl.carousel');
+        })
+        $('.home_sec4 .wrapp1 .arows_wrap .arrow.arrow2').click(function() {
+            owl2.trigger('next.owl.carousel');
+        })
+    });
+</script>
+
+
+<div class="home_sec5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="wrapper">
+                    <div class="arr_brn">
+                        <a href="#">View All</a>
+                    </div>
+
+                    <div class="btn_standard">
+                        <a href="#">Kontakt</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+<div class="home_sec6">
+    <div class="video-wrap">
+        <video disableRemotePlayback loop playsinline muted autoplay="">
+            <source src="<?php echo get_template_directory_uri(); ?>/img/home_content/Home/home-video-background-earth.mp4" type="video/mp4">
+        </video>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="title_wrap">
+                    <div class="subtitle">
+                        <h3>Discover Our Company</h3>
+                    </div>
+
+                    <div class="global_title">
+                        <h2 class="line1">Lorem Ipsum</h2>
+                        <h2 class="line2">Vision und Grundsätze</h2>
+                    </div>
+
+
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="wrap">
+                    <div class="title">
+                        <h2>Headline Vision</h2>
+                    </div>
+
+                    <div class="text">
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. mehr erfahren</p>
+                    </div>
+
+                    <div class="button_wrapper">
+                        <a href="#">mehr erfahren</a>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-lg-3">
+                <div class="wrap">
+                    <div class="title">
+                        <h2>Headline Grunsätze</h2>
+                    </div>
+
+                    <div class="text">
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. mehr erfahren</p>
+                    </div>
+
+                    <div class="button_wrapper">
+                        <a href="#">mehr erfahren</a>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-lg-3">
+                <div class="wrap">
+                    <div class="title">
+                        <h2>Headline Team</h2>
+                    </div>
+
+                    <div class="text">
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. mehr erfahren</p>
+                    </div>
+
+                    <div class="button_wrapper">
+                        <a href="#">mehr erfahren</a>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-lg-3">
+                <div class="wrap">
+                    <div class="title">
+                        <h2>Headline Worldwide</h2>
+                    </div>
+
+                    <div class="text">
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. mehr erfahren</p>
+                    </div>
+
+                    <div class="button_wrapper">
+                        <a href="#">mehr erfahren</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col_lg-12">
+
+                <!--slider sa godinama----->
+
+            </div>
+
+
+            <div class="col-lg-3">
+                <div class="wrap_line">
+                    <div class="title">
+                        <h2>Länder</h2>
+                    </div>
+
+                    <div class="number">
+                        45
+                    </div>
+
+                    <div class="line">
+                        <div class="red_line">
+                            <div class="circle_top"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-lg-3">
+                <div class="wrap_line">
+                    <div class="title">
+                        <h2>Betriebjahre</h2>
+                    </div>
+
+                    <div class="number">
+                        7500
+                    </div>
+
+                    <div class="line">
+                        <div class="red_line">
+                            <div class="circle_top"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="col-lg-3">
+                <div class="wrap_line">
+                    <div class="title">
+                        <h2>Schmelzleistung (t/J)</h2>
+                    </div>
+
+                    <div class="number">
+                        2.200.000
+                    </div>
+
+                    <div class="line">
+                        <div class="red_line">
+                            <div class="circle_top"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="col-lg-3">
+                <div class="wrap_line">
+                    <div class="title">
+                        <h2>Vorwärmleistung (t/J)</h2>
+                    </div>
+
+                    <div class="number">
+                        800.000
+                    </div>
+
+                    <div class="line">
+                        <div class="red_line">
+                            <div class="circle_top"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-12">
+                <div class="wrapper_btnn">
+                    <div class="btn_standard">
+                        <a href="#">Kontakt</a>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+</div>
+
+<div class="home_map_sec">
+    <div class="image">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/home_content/Home/home-worldmap1.png" alt="">
+
+    </div>
+</div>
 
 <div></div>
 <br>
